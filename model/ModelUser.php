f6f916304a60c0dca4e63844d0dc95d7c7a15b96
blo1258 <bulenttoruk@gmail.com>
2025-06-30 15:01:12 +0200

    modelUser

--- model/ModelUser.php
+++ model/ModelUser.php
@@ -1,8 +1,19 @@
 <?php
 
 class ModelUser {
+
+    private $db;
+
+    public function __construct(PDO $db) {
+        $this->db = $db;
+    }
+
     public function getUsers() : array {
-        
+
+        $query = "SELECT id, pseudo, email FROM user";
+        $req = $this->db->query($query);
+        $user = $req->fetchAll(PDO::FETCH_ASSOC);
+        return $user;
 
     }
 }
